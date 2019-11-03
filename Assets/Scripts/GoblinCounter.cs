77de67218d1d9cbf07761e4c5cde977a6112041c
Lucas B <Lucas.Boedeker@ue-germany.de>
2019-11-03 13:09:36 +0100

    Added Goblin Counter

---
+++ Assets/Scripts/GoblinCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinCounter : MonoBehaviour
+{
+    public static GoblinCounter instance;
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static void AddGoblin ()
+    {
+        if (instance != null)
+            instance.count++;
+    }
+
+    void OnGUI ()
+    {
+        GUI.Box(new Rect(100,100,100,100),"Goblis: " + count);
+    }
+}
