084d7547d78f47d554afbcaefae950d4e6c3c1ce
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-03 15:36:16 +0100

    added start scene

---
+++ Assets/Scripts/ButtonBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class ButtonBlock : MonoBehaviour, IEffectable
+{
+
+    [SerializeField] bool isExit;
+
+    [HideIf("isExit")]
+    [SerializeField] string sceneName;
+
+
+    public void Explode(Bomb source)
+    {
+        if (isExit)
+        {
+            Debug.Log("Application Quit");
+            Application.Quit();
+            Debug.Break();
+        }
+        else
+        {
+            Debug.Log("Loading scene: " + sceneName);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+
+   
+}
