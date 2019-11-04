46de6020d6364d334f8064288ac1be608bd60eea
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-04 22:20:40 +0100

    working on tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] Step[] steps;
+    int currentStep = -1;
+
+    [SerializeField] Transform light;
+
+    [Button]
+    void NextStep()
+    {
+        currentStep++;
+
+        if (currentStep >= steps.Length)
+            return;
+
+        Step step = steps[currentStep];
+
+        light.position = step.lightPosition.position;
+        
+
+    }
+
+
+
+
+    public class Step
+    {
+        public Transform lightPosition;
+
+        [TextArea(2,10)]
+        public string text;
+
+
+    }
+}
