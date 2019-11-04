8060ee7d4e626645372e58aff4257999de23b2d5
Lucas B <Lucas.Boedeker@ue-germany.de>
2019-11-04 13:43:41 +0100

    Added Grandpa and prepared him to be some sort of tutorial guy

---
+++ Assets/Granpa.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Granpa : MonoBehaviour
+{
+    [SerializeField]
+    AnimationClip intro;
+    [SerializeField]
+    AnimationClip talking;
+    [SerializeField]
+    AnimationClip outro;
+
+    Animator anim;
+
+    [Button]
+    public void PlayIntro()
+    {
+        PlayClip(intro);
+    }
+
+    [Button]
+    public void PlayTalk()
+    {
+        PlayClip(talking);
+    }
+
+    [Button]
+    public void PlayOutro()
+    {
+        PlayClip(outro);
+    }
+
+    void PlayClip (AnimationClip clip)
+    {
+        if (clip == null)
+        {
+            Debug.LogWarning("no clip assigned but tried to play one.");
+            return;
+        }
+
+        if (anim == null)
+            anim = GetComponent<Animator>();
+
+        anim?.Play(clip.name);
+    }
+}
